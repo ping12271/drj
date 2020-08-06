670c02e886750ea270391191ef42a7965ee6db4d
Hyeeun Shin <hyeeunshin@Hyeeunui-iMac.local>
2020-08-07 05:00:30 +0900

    ..

--- js/script.js
+++ js/script.js
@@ -77,7 +77,7 @@ var drjart = {
     },
 
     handleSideMenu: function () {
-        $('.all-menu, .close').on('click', function () {
+        $('.all-menu,.close').on('click', function () {
             $('html').toggleClass('open-side-menu');
         })
         $('.side-header .screen').on('click', function () {
@@ -147,7 +147,7 @@ var drjart = {
     renderProductBest: function () {
         const products = [
             {
-                a: "detail.html",
+                a: "https://ping12271.github.io/drj/pages/detail.html",
                 image: "../images/1560475092450.webp",
                 tags: "#탱글수분크림",
                 productName: "바이탈 하이드라 솔루션 바이옴 워터 크림",
@@ -158,6 +158,7 @@ var drjart = {
                 labels: ["new","best","gift"]
             },
             {
+                a: "https://ping12271.github.io/drj/pages/detail.html",
                 image: "../images/1591672265339.webp",
                 tags: "#프라이머선크림",
                 productName: "솔라바이옴™프라이머 특별기획세트",
@@ -168,6 +169,7 @@ var drjart = {
                 labels: [],
             },
             {
+                a: "https://ping12271.github.io/drj/pages/detail.html",
                 image: "../images/1587723450681.webp",
                 tags: "#시카베스트세트",
                 productName: "시카페어 취향저격 세트",
@@ -178,6 +180,7 @@ var drjart = {
                 labels: ["new", "best", "gift", "sale"],
             },
             {
+                a: "https://ping12271.github.io/drj/pages/detail.html",
                 image: "../images/1589418738580.webp",
                 tags: '#233억에센스+톤업샷"',
                 productName: "바이옴 에센스&블루 샷",
@@ -188,6 +191,7 @@ var drjart = {
                 labels: ["best", "gift", "sale"]
             },
             {
+                a: "https://ping12271.github.io/drj/pages/detail.html",
                 image: "../images/1589852789984.webp",
                 tags: "수분선앰플",
                 productName: "솔라바이옴™앰플 특별 기획세트",
@@ -247,6 +251,7 @@ var drjart = {
     renderProductBenefit: function () {
         const products = [
             {
+                a: "https://ping12271.github.io/drj/pages/detail.html",
                 image: "../images/1590393773811.webp",
                 tags: "#시카리페어세럼 ",
                 productName: "2세대 시카페어 세럼",
@@ -257,6 +262,7 @@ var drjart = {
                 labels: ["new","best","gift"]
             },
             {
+                a: "https://ping12271.github.io/drj/pages/detail.html",
                 image: "../images/1587020955660.webp",
                 tags: "#수분체질개선_에센스",
                 productName: "바이탈 하이드라 솔루션 바이옴 에센스 150mL",
@@ -267,6 +273,7 @@ var drjart = {
                 labels: ["best"]
             },
             {
+                a: "https://ping12271.github.io/drj/pages/detail.html",
                 image: "../images/1587022010571.webp",
                 tags: "# 함께해서 15% SALE",
                 productName: "티트리먼트™ 대용량 2종세트",
@@ -277,6 +284,7 @@ var drjart = {
                 labels: ["best"]
             },
             {
+                a: "https://ping12271.github.io/drj/pages/detail.html",
                 image: "../images/1563762254864.webp",
                 tags: "#탱글수분크림",
                 productName: "솔라바이옴™앰플 특별 기획세트",
@@ -287,6 +295,7 @@ var drjart = {
                 labels: ["best","gift","sale"]
             },
             {
+                a: "https://ping12271.github.io/drj/pages/detail.html",
                 image: "../images/1591944934738.webp",
                 tags: "#민감피부선크림",
                 productName: "솔라바이옴™징크림 특별 기획세트",
@@ -302,24 +311,27 @@ var drjart = {
             return `
                 <li>
                         <div class="item">
-                            <div class="thumb">
-                                <img src=${item.image} alt="">
-                                <div class="labels">
-                                ${item.labels.map(function (label) {
+                            <a href="${item.a}">
+                                <div class="thumb">
+                                    <img src=${item.image} alt="">
+                                    <div class="labels">
+                                    ${item.labels.map(function (label) {
                 return `<div class="${label}">${label}</div>`
             }).join("")}
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="description">
-                                <div class="tags">${item.tags}</div>
-                                <h3>${item.productName}</h3>
-                                <div class="price ${item.price.origin ? "sale":""}">
-                                    <div class="origin-price">${item.price.origin}</div>
-                                    <stron>${item.price.currentPrice}</stron>
+                                <div class="description">
+                                    <div class="tags">${item.tags}</div>
+                                    <h3>${item.productName}</h3>
+                                    <div class="price ${item.price.origin ? "sale" : ""}">
+                                        <div class="origin-price">${item.price.origin}</div>
+                                        <strong>${item.price.currentPrice}</strong>
+                                    </div>
                                 </div>
-                            </div>
+                            </a>
                             <div class="buttons">
-                                <a class="buy">바로가기
+                                <a href="#!" class="buy">
+                                    바로구매
                                     <span class="icon-plus"></span>
                                     <span class="slide-plus">
                                         <span class="screen"></span>
